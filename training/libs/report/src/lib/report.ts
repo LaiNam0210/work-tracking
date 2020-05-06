a7e6c638a026d2f9984287a12bd6721857dab5ee
Gia Bao <nggbaobkit@gmail.com>
2020-05-06 18:11:19 +0700

    issue/#12

--- training/libs/report/src/lib/report.ts
+++ training/libs/report/src/lib/report.ts
@@ -5,14 +5,25 @@ export class Report {
   jobToday: string;
   timeCreated: string;
 
-  constructor(id: string, jobYesterday: string, problems: string, jobToday: string) {
+  constructor(
+    id: string,
+    jobYesterday: string,
+    problems: string,
+    jobToday: string
+  ) {
     this.id = id;
     this.jobYesterday = jobYesterday;
     this.problems = problems;
     this.jobToday = jobToday;
     const today = new Date();
-    const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-    const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    const date =
+      today.getFullYear() +
+      '-' +
+      (today.getMonth() + 1) +
+      '-' +
+      today.getDate();
+    const time =
+      today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
     this.timeCreated = date + ' ' + time;
   }
 }
