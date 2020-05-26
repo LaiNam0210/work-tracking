0376a80bcfae2f06ba5b37c324f5fb8f4c2f5144
Gia Bao <nggbaobkit@gmail.com>
2020-05-26 10:11:17 +0700

    issue/#20 Implemented UI for login form.

--- training/apps/work-tracking/src/app/auth-page/auth-page.component.ts
+++ training/apps/work-tracking/src/app/auth-page/auth-page.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ReportFacade } from '@training/store/report';
 import { Observable } from 'rxjs';
+import { FormBuilder, FormGroupDirective, Validators } from '@angular/forms';
 
 @Component({
   selector: 'training-auth-page',
@@ -9,17 +10,26 @@ import { Observable } from 'rxjs';
   styleUrls: ['./auth-page.component.scss']
 })
 export class AuthPageComponent implements OnInit {
+  loginForm = this.fb.group({
+    username: [null, Validators.required],
+    password: [null, Validators.required]
+  });
+  @ViewChild(FormGroupDirective) loginNgForm;
   authResult$: Observable<{ accessToken: string }>;
 
   constructor(
     private httpClient: HttpClient,
-    private reportFacade: ReportFacade
+    private reportFacade: ReportFacade,
+    private fb: FormBuilder
   ) {}
 
   ngOnInit(): void {}
 
   login() {
-    console.log('Login!');
-    this.reportFacade.login('john', 'changeme');
+    this.reportFacade.login(
+      this.loginForm.value.username,
+      this.loginForm.value.password
+    );
+    this.loginNgForm.resetForm();
   }
 }
